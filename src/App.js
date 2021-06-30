843ef26f19a15da404ab961a0fa7f07ccdb4d485
joaobenthin <joao.benthin@dtidigital.com.br>
2021-06-30 20:04:05 -0300

    requisicao com api rest

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { Header } from './components/Header';
 
 function App() {
+  function handleClick() {
+    fetch('http://localhost:3333')
+    .then((response) => response.json()
+    .then((data) => console.log(data.status))
+    );
+  }
+  
+
   return (
     <>
       <CssBaseline />
+      <button onClick={handleClick}>Vem aqui ciclo</button>
       <Header />
     </>
   );
